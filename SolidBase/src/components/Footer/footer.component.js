22d145f9ca274fbd36a96e5dc787fb88b2513e9f
astrid <UO258194@uniovi.es>
2020-01-24 18:19:26 +0100

    botones amigos redirige

--- SolidBase/src/components/Footer/footer.component.js
+++ SolidBase/src/components/Footer/footer.component.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { IconLookup, IconDefinition, findIconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { withTranslation } from 'react-i18next';
 
 type Props = {
@@ -9,15 +7,15 @@ type Props = {
 
 const Footer = (props: Props) => {
   const { t } = props;
-  const githubIcon: IconLookup = { prefix: 'fab', iconName: 'github' };
-  const githubIconDef: IconDefinition = findIconDefinition(githubIcon);
+  //const githubIcon: IconLookup = { prefix: 'fab', iconName: 'github' };
+  //const githubIconDef: IconDefinition = findIconDefinition(githubIcon);
 
   return (
     <footer className="solid-footer footer">
       <section className="solid-footer__content">
         <div className="solid-footer__content--copyright">
           <ul>
-            <li>© {process.env.REACT_APP_COMPANY_NAME}</li>
+            <li>© empathy.co</li>
             <li>
               {t('footer.version')}
               {''} <span className="build-value">{process.env.REACT_APP_VERSION}</span>
@@ -27,26 +25,7 @@ const Footer = (props: Props) => {
 
         <div className="solid-footer__content--links">
           <ul>
-            <li>
-              <a
-                href="https://github.com/Inrupt-inc/solid-react-sdk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon className="link-icon" icon={githubIconDef} />
-                react-solid-sdk
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/Inrupt-inc/solid-style-guide"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FontAwesomeIcon className="link-icon" icon={githubIconDef} />
-                solid-style-guide
-              </a>
-            </li>
+
           </ul>
         </div>
       </section>
--- SolidBase/src/containers/FriendList/friendlist.component.js
+++ SolidBase/src/containers/FriendList/friendlist.component.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { CenterContainer } from '@util-components';
+//import { CenterContainer } from '@util-components';
+//import { ProviderLogin, withWebId } from '@inrupt/solid-react-components';
+import {List,Value,useLDflexList,useLDflexValue, useWebId,  Name} from '@solid/react';
+import {solid} from '@solid/query-ldflex';
+
+function friends() {
+  /*let friends   = solid.data[useWebId()].friends
+  const doc   = solid.data['https://jeffz.solid.community/public/ldflex.ttl']
+  console.log(Name(friends))*/
+  const friends = useLDflexList('user.friends');
+  let size = friends.size
+  return friends
+}
+function viewProfile(profile) {
+  window.open(profile)
+ }
+function friendlist() {
+  const name = useLDflexValue('user.name') || 'unknown';
+
+  const friends = useLDflexList('user.friends');
+
+   let fmap = friends.map(function(friend) {
+    let url = <a href={friend.toString()}>{friend.toString()} </a>
+    return   url
+  })
+   return <p> {name.toString()} friends  are:    </p>
+}
+
 
 const FriendListComponent = () => {
-  return (<p>This is a list of friends</p>);
-  
+  return (<p>
+       This is {[useWebId()]} a list of friends {friendlist()}
+
+
+      <List src={friends()}>{friend =>
+        <li key={friend}>
+          <button onClick={() => viewProfile(friend.toString())}>
+            <Value src={`[${friend}].name`}> {`${friend}`}</Value>
+          </button>
+        </li>}
+      </List>
+  </p>
+  );
+
 }
 export default FriendListComponent;
+
