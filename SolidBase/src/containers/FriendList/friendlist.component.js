b41b0693383e603ec85741884b596dbdb8afdd50
Iván Prieto Labrador <uo258306@uniovi.es>
2020-01-27 11:55:57 +0100

    Solved conflicts
    Solved conflicts that emerged when merging the pull request

--- SolidBase/src/containers/FriendList/friendlist.component.js
+++ SolidBase/src/containers/FriendList/friendlist.component.js
@@ -2,14 +2,23 @@ import React, { useState } from "react";
 import { RdfService } from "@services";
 import auth from "solid-auth-client";
 import * as rdf from "rdflib";
-import {List, Image,Value,useLDflexList,useLDflexValue, useWebId,  Name} from '@solid/react';
-import {lib} from '@solid/query-ldflex';
-import {ImageProfile} from '/components/ImageProfile/image-profile.component';
+import {
+  List,
+  Image,
+  Value,
+  useLDflexList,
+  useLDflexValue,
+  useWebId,
+  Name
+} from "@solid/react";
+import { lib } from "@solid/query-ldflex";
+import { ImageProfile } from "/components/ImageProfile/image-profile.component";
 import data from "@solid/query-ldflex";
-const $rdf = require('rdflib');
 import { solid } from "@solid/query-ldflex";
 import { rdfService } from "@services";
-import {FriendListItem} from "@components";
+import { FriendListItem } from "@components";
+
+const $rdf = require("rdflib");
 
 //import { CenterContainer } from '@util-components';
 //import { ProviderLogin, withWebId } from '@inrupt/solid-react-components';
@@ -27,47 +36,48 @@ function friends() {
   /*let friends   = solid.data[useWebId()].friends
   const doc   = solid.data['https://jeffz.solid.community/public/ldflex.ttl']
   console.log(Name(friends))*/
-  const friends = useLDflexList('user.friends');
-  let size = friends.size
+  const friends = useLDflexList("user.friends");
+  let size = friends.size;
 
-        // the raw default JSON-LD context
+  // the raw default JSON-LD context
 
-
-  return friends
+  return friends;
 }
 function deletefriend(profile) {
-  if (window.confirm("Do you really want to delete "+profile+" from your friends?")) {
-      let FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/'); // n0
+  if (
+    window.confirm(
+      "Do you really want to delete " + profile + " from your friends?"
+    )
+  ) {
+    let FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/"); // n0
 
-      let insertions = [];
-      let deletions = [];
-      let docs = "https://astrid.solid.community/profile/card#me";
-      console.log(docs.split('#')[0]+ ":" + docs.toString())
-      const doc = $rdf.sym(docs);
-      let subject = $rdf.sym(docs.toString());
+    let insertions = [];
+    let deletions = [];
+    let docs = "https://astrid.solid.community/profile/card#me";
+    console.log(docs.split("#")[0] + ":" + docs.toString());
+    const doc = $rdf.sym(docs);
+    let subject = $rdf.sym(docs.toString());
 
-      let predicate = $rdf.sym(FOAF('knows'));
-      let object = $rdf.sym(profile);
-      let statement = $rdf.st(subject, predicate, object, doc);
-      insertions.push(statement);
-      $rdf.addfriend(docs);
-    }
+    let predicate = $rdf.sym(FOAF("knows"));
+    let object = $rdf.sym(profile);
+    let statement = $rdf.st(subject, predicate, object, doc);
+    insertions.push(statement);
+    $rdf.addfriend(docs);
   }
-  function webid() {
-    const user = useLDflexValue('user') || 'unknown';
-    return user;
-  }
+}
+function webid() {
+  const user = useLDflexValue("user") || "unknown";
+  return user;
+}
 function friendlist() {
   const name = useLDflexValue("user.name") || "unknown";
 
   const friends = useLDflexList("user.friends");
-
 }
-function  proimage() {
-  const img = useLDflexValue('user.image');
-return img + "";
+function proimage() {
+  const img = useLDflexValue("user.image");
+  return img + "";
 }
-function profilephoto( ) {
 
 export default class FriendListComponent extends React.Component {
   constructor() {
@@ -91,11 +101,13 @@ export default class FriendListComponent extends React.Component {
       <div>
         {this.state.friends.map(friend => {
           return (
-         <FriendListItem user = {friend.value}></FriendListItem>
-         <button onClick={() => deletefriend(friend.value)}>
-            <Value src={friend.value}>  Delete Friend</Value>
-          </button>
-        );
+            <div>
+              <FriendListItem user={friend.value}></FriendListItem>
+              <button onClick={() => deletefriend(friend.value)}>
+                <Value src={friend.value}> Delete Friend</Value>
+              </button>
+            </div>
+          );
         })}
         ;
       </div>
@@ -107,6 +119,4 @@ export default class FriendListComponent extends React.Component {
       </div>
     );
   }
-} 
-
-
+}
